0779f16daaf38c925a5155e05dc87743f4591c38
Martin Wimpress <code@flexion.org>
2020-11-10 22:34:29 +0000

    Update Ubuntu install script

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -xe
 echo "Installing libraries"
-sudo apt install qt5-default libhidapi-hidraw0 libudev-dev libusb-1.0-0-dev python3-pip
+sudo apt -y install libhidapi-libusb0 libudev-dev libusb-1.0-0-dev python3-pip
+sudo apt -y install python3-evdev python3-hid python3-pil python3-six python3-xlib
+
 echo "Adding udev rules and reloading"
 sudo usermod -a -G plugdev `whoami`
-
 sudo tee /etc/udev/rules.d/99-streamdeck.rules << EOF
 SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0060", MODE:="666", GROUP="plugdev"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0063", MODE:="666", GROUP="plugdev"
@@ -12,8 +13,8 @@ SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006d", MODE:="666"
 EOF
 
 sudo udevadm control --reload-rules
+sudo udevadm trigger
 
-echo "Unplug and replug in device for the new udev rules to take effect"
 echo "Installing streamdeck_ui"
-pip3 install --user streamdeck_ui
+pip3 install --user https://github.com/flexiondotorg/streamdeck-ui/archive/master.zip
 echo "If the installation was successful, run 'streamdeck' to start."
