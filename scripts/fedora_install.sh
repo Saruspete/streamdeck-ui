038fe3a96408f1a141e094768fb10987d54f1b96
Martin Wimpress <code@flexion.org>
2020-11-10 22:34:40 +0000

    Update Fedora install script

--- scripts/fedora_install.sh
+++ scripts/fedora_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -xe
 echo "Installing libraries"
 sudo dnf install python3-devel libusb-devel libusbx-devel libudev-devel systemd-devel
+
 echo "Adding udev rules and reloading"
 sudo usermod -a -G plugdev `whoami`
-
 sudo tee /etc/udev/rules.d/99-streamdeck.rules << EOF
 SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0060", MODE:="666", GROUP="plugdev"
 SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0063", MODE:="666", GROUP="plugdev"
@@ -12,8 +12,8 @@ SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006d", MODE:="666"
 EOF
 
 sudo udevadm control --reload-rules
+sudo udevadm trigger
 
-echo "Unplug and replug in device for the new udev rules to take effect"
 echo "Installing streamdeck_ui"
-pip3 install --user streamdeck_ui
+pip3 install --user https://github.com/flexiondotorg/streamdeck-ui/archive/master.zip
 echo "If the installation was successful, run 'streamdeck' to start."
